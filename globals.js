87295a86e9ba8e53fd9b2c52507811d9a9e6fcba
Palle Cogburn <palle@4thex.com>
2015-06-30 06:10:43 -0700

    Added new file for globals

---
+++ globals.js
@@ -0,0 +1,5 @@
+var Chess = Chess || {};
+Chess.Colors = {White: 0, Black: 1};
+Chess.Kinds = {K: 0,	Q: 1, B: 2,	N: 3, R: 4, P: 5};
+Chess.Files = {a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7};
+Chess.Ranks = {1: 0, 2: 1, 3: 2, 4: 3, 5: 4, 6: 5, 7: 6, 8: 7};
